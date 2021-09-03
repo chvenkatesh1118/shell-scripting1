b36f72fd441e2513e83b906a1f2413353b58f71c
Raghu K <raghudevopsb58@gmail.com>
2021-09-03 01:42:36 +0000

    Making changes to MongoDB Script

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -9,7 +9,11 @@ Status_Check() {
   fi 
 }
 
-echo "Setting UP MongoDB Repo"
+Print() {
+  echo -n -e "$1"
+}
+
+Print "Setting UP MongoDB Repo"
 
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -19,30 +23,30 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 Status_Check $?
 
-echo "Installing MongoDB"
+Print "Installing MongoDB"
 yum install -y mongodb-org &>>/tmp/log
 Status_Check $?
 
-echo "Configuring MongoDB"
+Print "Configuring MongoDB"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 Status_Check $?
 
-echo "Starting MongoDB"
+Print "Starting MongoDB"
 systemctl enable mongod
 systemctl restart mongod
 Status_Check $?
 
-echo "Downloading MongoDB Schema"
+Print "Downloading MongoDB Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 Status_Check $?
 
 cd /tmp
-echo "Extracting Schema Archive"
+Print "Extracting Schema Archive"
 unzipp -o mongodb.zip &>>/tmp/log
 Status_Check $?
 
 cd mongodb-main
-echo "Loading Schema"
+Print "Loading Schema"
 mongo < catalogue.js &>>/tmp/log
 mongo < users.js  &>>/tmp/log
 Status_Check $?
