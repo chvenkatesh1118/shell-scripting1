b63903fae5fb7e131fe126516ec8e902753ee64d
Raghu K <raghudevopsb58@gmail.com>
2021-09-04 00:43:46 +0000

    Making changes to Catalogue Script

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,31 @@
 #!/bin/bash 
 
 source components/common.sh
+
+Print "Installing NodeJS"
+yum install nodejs make gcc-c++ -y 
+Status_Check $? 
+
+Print "Adding RoboShop User"
+useradd roboshop &>>$LOG 
+Status_Check $? 
+
+Print "Downloading Catalogue Content"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG 
+Status_Check $? 
+
+Print "Extracting Catalogue"
+cd /home/roboshop
+unzip -o /tmp/catalogue.zip &>>$LOG 
+mv catalogue-main catalogue
+Status_Check $?
+
+cd /home/roboshop/catalogue
+npm install &>>$LOG 
+
+
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
