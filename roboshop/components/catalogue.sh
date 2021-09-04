43565f5ad2c08b2eec0f545615cae2b4bb8895d4
KalluruS <>
2021-09-04 07:14:40 +0530

    Update Catalogue script

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -31,6 +31,10 @@ Status_Check $?
 
 chown roboshop:roboshop -R /home/roboshop 
 
+Print "Update SystemD Service"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
+Status_Check $?
+
 Print "Setup SystemD Service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl start catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
 Status_Check $?
