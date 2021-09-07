0562262fac26bf8feb7f6eaadb9d186dbc10d387
KalluruS <>
2021-09-07 06:57:23 +0530

    Update shell-scripting/

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -45,7 +45,7 @@ DOWNLOAD() {
 
 SystemdD_Setup() {
   Print "Update SystemD Service\t"
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e  's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
   Status_Check $?
 
   Print "Setup SystemD Service\t"
