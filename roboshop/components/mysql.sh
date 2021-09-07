5efe2ef4db54f5a40c3386ada262b3044c34c169
KalluruS <>
2021-09-07 07:18:20 +0530

    Update shell-scripting/

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -21,9 +21,9 @@ Status_Check $?
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 echo 'ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';' >/tmp/reset.sql
-mysql -u root -p"${DEFAULT_PASSWORD}" </tmp/reset.sql
+mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/reset.sql
 
-exit 
+exit
 
 
 
