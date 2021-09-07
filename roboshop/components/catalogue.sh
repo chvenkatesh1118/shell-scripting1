8cc1348aee9cfd691093a3b01e2ea234660d99ee
KalluruS <>
2021-09-07 06:19:09 +0530

    Update shell-scripting/

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,11 +2,11 @@
 
 source components/common.sh
 
-Print "Installing NodeJS"
+Print "Installing NodeJS\t"
 yum install nodejs make gcc-c++ -y &>>$LOG 
 Status_Check $? 
 
-Print "Adding RoboShop User"
+Print "Adding RoboShop User\t"
 id roboshop &>>$LOG 
 if [ $? -eq 0 ]; then 
   echo "User already there, So Skipping" &>>$LOG 
@@ -19,7 +19,7 @@ Print "Downloading Catalogue Content"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG 
 Status_Check $? 
 
-Print "Extracting Catalogue"
+Print "Extracting Catalogue\t"
 cd /home/roboshop
 rm -rf catalogue && unzip -o /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue
 Status_Check $?
@@ -31,10 +31,10 @@ Status_Check $?
 
 chown roboshop:roboshop -R /home/roboshop 
 
-Print "Update SystemD Service"
+Print "Update SystemD Service\t"
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
 Status_Check $?
 
-Print "Setup SystemD Service"
+Print "Setup SystemD Service\t"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl restart catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
 Status_Check $?
