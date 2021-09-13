d682601b4e1fbe6d8cabb7ed8f404c23908f08d3
KalluruS <>
2021-09-13 07:10:05 +0530

    Update shell-scripting/

---
+++ 10-loops.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## If we need to execute a set of commands continously in a iterative way then we use loops.
+
+# while  (Inverse logic of while is until loop)
+# for    (Inverse logic of for is select loop)
+
+# while [ expression ]; do
+  # Commands
+# done
+
+i=10
+while [ $i -gt 0 ]; do
+  echo "Hello"
+  i=$(($i-1))
+done
