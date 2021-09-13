6e3ef62d59d602910a60537a8c450492048207b5
KalluruS <>
2021-09-13 06:17:56 +0530

    Update shell-scripting/

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -45,7 +45,7 @@ DOWNLOAD() {
 
 SystemdD_Setup() {
   Print "Update SystemD Service\t"
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e  's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e  's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/'  /home/roboshop/${COMPONENT}/systemd.service
   Status_Check $?
 
   Print "Setup SystemD Service\t"
@@ -81,4 +81,5 @@ JAVA() {
   mv target/shipping-1.0.jar shipping.jar &>>$LOG
   Status_Check $?
   chown roboshop:roboshop -R /home/roboshop
+  SystemdD_Setup
 }
