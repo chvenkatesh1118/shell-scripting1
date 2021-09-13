33c5698e5393d5ab125b22206f81308e1144363c
KalluruS <>
2021-09-13 06:12:52 +0530

    Update shell-scripting/

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -66,3 +66,19 @@ NODEJS() {
   chown roboshop:roboshop -R /home/roboshop
   SystemdD_Setup
 }
+
+JAVA() {
+  Print "Installing Maven\t"
+  yum install maven -y &>>$LOG
+  Status_Check $?
+  ADD_APP_USER
+  DOWNLOAD
+  cd /home/roboshop/shipping
+  Print "Make Shipping Package\t"
+  mvn clean package &>>$LOG
+  Status_Check $?
+  Print "Rename Shipping Package"
+  mv target/shipping-1.0.jar shipping.jar &>>$LOG
+  Status_Check $?
+  chown roboshop:roboshop -R /home/roboshop
+}
---
+++ roboshop/components/shipping.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source components/common.sh
+
+COMPONENT=catalogue
+## NODEJS is a function from common.sh
+JAVA
